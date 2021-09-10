a592a2cf755afe139a4d197bfc970f3d0fa5bd51
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-09-11 00:17:31 +0900

    Update Part2-Data-Visualization.R

--- Part2-Data-Visualization.R
+++ Part2-Data-Visualization.R
@@ -1,5 +1,4 @@
-#1 Pie Chart
-#: categorical data, 성별 변수 등
+
 
 #데이터 불러오기
 mydata <- read.csv("https://www.theissaclee.com/ko/courses/rstat101/examscore.csv", header = TRUE)
@@ -15,7 +14,9 @@ mytable #1행2열의 행렬로 볼 수 있음
 names(mytable) <- c("Female", "Male") #변수명을 변경
 mytable
 
-#파이차트만들기 
+#1 Pie Chart ---------------------------------------
+#: categorical data, 성별 변수 등
+
 pie(mytable, 
     main = "Student Gender") #차트명= main
 sum(mytable) 
@@ -23,19 +24,34 @@ c(10, 20) / 30 #퍼센테이지 구하기
 text(0.3, 0.3, "33.3%") #플롯에 텍스트 입력
 text(-0.3, -0.3, "66.7%") 
 
-#Stem and leaf plot
+#Stem and leaf plot -------------------------------
 stem(mydata$midterm)
 stem(mydata$midterm, scale = 2) #scale: 단위 늘리기(이 경우 10단위를 5단위로 세분화)
 
-#Histogram 
+#Histogram ----------------------------------------
 mydata$midterm
 hist(mydata$midterm, breaks = c(0, 20, 40, 60, 80),
      main = "MIDTERM")
 
 hist(mydata$midterm, breaks = c((0:4)*20))
 hist(mydata$midterm, breaks = c((0:16)*5),
-     mian = "Midterm Distribution",
+     mian = "Distribution of Midterm Result",
      xlab = "Score",
      ylab = "Frequency") #5단위로 히스토그램을 그리고 싶고, 맥시멈 값은 80일 때
+
+#Box plot (r/ to Median) --------------------------
+boxplot(mydata$midterm, 
+        main = "Distribution of Midterm Result",
+        xlab = "Score",
+        horizontal = T)
+
+#outlier 표시하기
+mydata$midterm[1] <- 100
+
+boxplot(mydata$midterm, 
+        mydata$final, 
+        main = "Distribution of Midterm Result",
+        xlab = "Score",
+        horizontal = T)
 
 

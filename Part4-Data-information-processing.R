83d47d5c2f3fb0343dc97832942c2e4d9de696e0
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-09-12 21:25:07 +0900

    Update Part4-Data-information-processing.R

--- Part4-Data-information-processing.R
+++ Part4-Data-information-processing.R
@@ -59,3 +59,52 @@ statMode <- function(data_vec){
 
 statMode(x)
 
+
+#---------------------------------
+#OPERATOR 논리연산자 (==, > / <, >= / <=)
+
+TRUE == 1
+FALSE == 0
+
+TRUE * TRUE == 1 #TRUE
+TRUE * FALSE == 0 #FALSE
+
+#AND, OR
+(2 < 3) & (2 < 5) #TRUE
+(2 > 3) & (2 < 5) #FALS
+
+TRUE | TRUE #TRUE
+TRUE | FALSE #TRUE
+FALSE | TRUE #TRUE
+FALSE | FALSE #FALSE
+
+#How to Make "Categorical Variables" by Filtering Vector 
+x <- c(1, 3, 9, 7, 1)
+x[c(TRUE, FALSE, FALSE, TRUE, TRUE)] #Filtering True value in x
+x[x <= 3] <- 5 #TRUE 값인 1, 7, 1 자리에 5를 입력 
+
+
+x <- c(1, 3, 9, 7, 1)
+
+y <- character(length = 5) #5개의 공란 생성
+y <- c("1st", "2nd", "3rd", "4th", "5th")
+
+y[x <= 3] <- "<= 3" 
+y[x > 3] <- " >3"
+y #"<= 3" "<= 3" " >3"  " >3"  "<= 3"
+
+mydata <- read.csv("https://www.theissaclee.com/ko/courses/rstat101/examscore.csv", header = TRUE)
+head(mydata)
+summary(mydata$midterm) #"summary" returns Min./1st Qu./Median/Mean/3rd Qu./Max. 
+y <- character(length = 30) #30개의 공란 만들기
+y[mydata$midterm < 49.75] <- "S"
+y[mydata$midterm >= 49.75] <- "U"
+y
+
+mydata$SU <- y
+head(mydata)
+
+mytable <- table(mydata$SU)
+pie(mytable, 
+    labels = names(mytable), 
+    main = "Pie Chart of the S/U")

f5ccf17bb03c5a6b035473a2448b605133e73621
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-09-15 16:08:59 +0900

    Update Part7-Noise-n-irregulartiy.R

---
+++ Part7-Noise-n-irregulartiy.R
@@ -0,0 +1,81 @@
+#original straight
+true_fuc <- function(x){
+  result <- x * 3 + 20
+  return(result)
+}
+
+range_vec <- 1:20
+plot(range_vec, true_fuc(range_vec), 
+     type = "l", lty = 3)
+
+#when w/o noise: points(x, y) 관찰 #잡음 없는 직선
+points(4, true_fuc(4), pch = 19)
+points(10, true_fuc(10), pch = 19)
+x <- 5:15
+points(x, true_fuc(x), pch = 19)
+
+#when w/ number noise! #잡음
+range_vec <- 1:20
+plot(range_vec, true_fuc(range_vec), 
+     type = "l", lty = 3)
+
+noise_fuc <- function(x){
+  result <- x * 3 + 20 + 4
+  return(result)
+}
+
+points(x, noise_fuc(x), pch = 19)
+
+#noise w/ some Rules! #규칙성 있음
+range_vec <- 1:20
+plot(range_vec, true_fuc(range_vec), 
+     type = "l", lty = 3)
+
+x <- 5:15
+noise_fuc2 <- function(x){
+  true_vec <- x * 3 + 20 + 0
+  noise_vec <- rep(0, length(x))
+  noise_vec[x %% 2 == 0] <- 3
+  noise_vec[x %% 2 == 1] <- -5
+  
+  result <- true_vec + noise_vec
+  return(result)
+}
+
+#points(5, noise_fuc2(4), pch = 19)
+#points(9, noise_fuc2(9), pch = 19)
+#x <- 5:15
+#points(x, noise_fuc2(x), pch = 19)
+
+#Randomly generator 0(.0000) ~ 0.9999
+#소숫점 6자리까지 시간 노티.
+options(digits.secs = 6) 
+Sys.time()
+1378 / 10000
+6585 / 10000
+2936 / 10000
+6332 / 10000
+
+mode(Sys.time()) #Sys.time()값은 숫자벡터(numeric)다.
+
+range_vec <- 0:0.999
+plot(range_vec, true_fuc(range_vec), 
+     type = "l", lty = 3)
+
+#Sys.time()값의 마지막 4자리를 가져오는 함수 생성
+random_0to1 <- function(){
+  options(digits.secs = 6)
+  current_time <- as.character(Sys.time())
+                  #Sys.time()값을 문자열로 변환
+  n <- nchar(current_time)  #nchar : number of character
+  time_decimal <- substr(current_time, n-3, n) #substr(벡터, 시작위치, 끝위치)
+  return(as.integer(time_decimal) / 10000) #as.integer: 정수로 변환 
+}
+
+a <- random_0to1()
+points(a, noise_fuc2(a))
+
+#runif(10) #0~1 랜덤값 n개 생성
+runif(10) * 5 + 5 # 5~10의 랜덤값 10개 생성
+runif(10) * 4 + 15 # 15~19의 랜덤값 10개 생성
+

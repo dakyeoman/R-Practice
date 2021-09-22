55449c4912170d0e48945e74605a14f90a70ff89
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-09-23 00:38:48 +0900

    Update Part9.R

--- Part9.R
+++ Part9.R
@@ -13,5 +13,13 @@ variation <- rnorm(length(age)) * 3
 weight <- true_func(age) + variation
 
 plot(age, weight, pch = 19, ylim = c(5, 50), xlim = c(2, 10))
-abline(10, 3, col = "white")
+abline(10, 3, col = "black")
+
+#실제의 데이터 조사: 5명을 모집
+mysample_age <- sample(seq(2, 10, by = 1/12), 5, replace = TRUE)
+mysample_weight <- true_func(mysample_age) + rnorm(length(mysample_age)) * 3
+points(mysample_age, mysample_weight, pch = 16, col = "green")
+#주어진 데이터를 사용한 회귀 직선 계산: lm()
+result <- lm(mysample_weight ~ mysample_age) #(종속변수 ~ 독립변수)
+abline(result, col = "green")
 

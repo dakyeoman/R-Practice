760036b3848447f8fbfc32b6685663ed84eeae85
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-11-08 23:20:11 +0900

    Create Explore Kaggle.R

---
+++ Explore Kaggle.R
@@ -0,0 +1,9 @@
+tokyo_tweet <- read.csv(file.choose()) #csv_exam.csv
+table(tokyo_tweet)
+tokyo_tweet
+
+
+tokyo_data<- read.csv(file.choose()) #csv_exam.csv
+tokyo_data
+colnames(tokyo_tweet)
+tokyo_tweet$hashtags
